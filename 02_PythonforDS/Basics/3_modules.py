17361ea73abf40c65dc24bb86d2c9a63d59a1264
Amit Saha <justamitsaha@gmail.com>
2026-07-19 14:07:13 +0530

    Back up

--- 02_PythonforDS/Basics/3_modules.py
+++ 02_PythonforDS/Basics/3_modules.py
@@ -5,9 +5,30 @@ import math
 #math is a module dir is a built in function that returns a sorted list of strings containing the names defined by modules
 #print(dir(math))
 
-print(7.6+8.7)
+# print(7.6+8.7)
 
-x = "Hello World"
-print(x[0])
-print(x[-1])
-print(type(x[3]))
+# x = "Hello World"
+# print(x[0])
+# print(x[-1])
+# print(type(x[3]))
+
+# i = [12, 9, 14]
+# k = [7, 16, 11]
+
+# for j in i[:]: for m in k[:]:
+#     if j % m == 0:
+#         j = j // 2
+#         m = m / 2
+#         print(j, m)
+#     else:
+#         j = j + m
+#         m = m - j
+#         print(j, m)
+
+
+sum([i**2 for i in range(10)])
+
+print(math.sqrt(16))
+
+
+
